ab18ffe263c4cfaebeef5556c3be3632b8001646
Baran Üçdağ <75309438+baranucdag@users.noreply.github.com>
2022-01-12 21:34:16 +0300

    the error on product entity fixed

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -9,7 +9,7 @@ namespace Entities.Conscrete
 {
     public class Product:IEntity
     {
-        public int ProductId { get; set; }
+        public int ProductId { get; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
         public short UnitsInStock { get; set; }
